c583769da14bda31368bc797b34b8e593c823cdb
C# <80015177+6Ione@users.noreply.github.com>
2021-09-01 18:57:53 -0600

    Add files via upload

---
+++ Ugly_Login_GUI/Ugly_Login_GUI/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ugly_Login_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string name = wc.DownloadString("https://pastebin.com/raw/JMNLkf56");
+            string pass = wc.DownloadString("https://pastebin.com/raw/xD9kkD0G");
+
+            if (name.Contains(textBox1.Text) && pass.Contains(pass))
+            {
+                Main main = new Main();
+                main.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Login failed!");
+            }
+        }
+    }
+}
---
+++ Ugly_Login_GUI/Ugly_Login_GUI/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ugly_Login_GUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string ip = wc.DownloadString("http://checkip.amazonaws.com/");
+            MessageBox.Show($"IP: {ip}\nPC Name: {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}", "Goodbye!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
